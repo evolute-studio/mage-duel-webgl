0f16298367c9daeb8203781edadf5ac48ed2e7d0
liyard-tls <mckinley55X@gmail.com>
2025-04-18 18:48:30 +0300

    brotli support

--- mage-duel-nextjs/next.config.ts
+++ mage-duel-nextjs/next.config.ts
@@ -2,6 +2,37 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  async headers() {
+    return [
+      {
+        source: "/:path*.br",
+        headers: [
+          {
+            key: "Content-Encoding",
+            value: "br",
+          },
+          {
+            key: "Content-Type",
+            value: "application/javascript",
+          },
+        ],
+      },
+    ];
+  },
+  webpack: (config) => {
+    config.module.rules.push({
+      test: /\.br$/,
+      use: [
+        {
+          loader: "file-loader",
+          options: {
+            name: "[name].[ext]",
+          },
+        },
+      ],
+    });
+    return config;
+  },
 };
 
 export default nextConfig;
