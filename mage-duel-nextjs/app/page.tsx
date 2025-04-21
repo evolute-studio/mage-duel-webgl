48786521b6dd2e4a680ba61bc0268a1349af1796
liyard-tls <mckinley55X@gmail.com>
2025-04-21 21:17:31 +0300

    new events (screen_time)

--- mage-duel-nextjs/app/page.tsx
+++ mage-duel-nextjs/app/page.tsx
@@ -1,17 +1,25 @@
 'use client';
 
 import { Analytics } from "@vercel/analytics/react"
-import { track } from '@vercel/analytics';
 import Head from 'next/head';
 import { useEffect } from "react";
-import { logEvent } from '../lib/axiom';
 import '../lib/error-handler';
+import { GameLoaded } from '../lib/events';
+import { initScreenTimeTracking, stopScreenTimeTracking } from '../lib/events';
+
+// 1 minute
+const screenTimeInterval = 1000 * 60;
 
 export default function Home() {
   useEffect(() => {
-    track("page_view");
-    logEvent('page_loaded', { page: 'home' });
-  }, [])
+    GameLoaded();
+    initScreenTimeTracking();
+
+    return () => {
+      stopScreenTimeTracking();
+    };
+  }, []);
+
   return (
     <>
       <Analytics />
