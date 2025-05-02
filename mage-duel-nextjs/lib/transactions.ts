47d069a62a910ca4920162960f36f39b0a294b5b
liyard-tls <mckinley55X@gmail.com>
2025-05-02 11:57:43 +0300

    all contracts methods

--- mage-duel-nextjs/lib/transactions.ts
+++ mage-duel-nextjs/lib/transactions.ts
@@ -1,12 +1,108 @@
-const EVOLUTE_DUEL_GAME_ADDRESS =
-  '0x039344460b0d9917c58185b47ce77848a6b9d5f0b26639f9faad2cdcb9451239'
-const EVOLUTE_DUEL_PLAYER_PROFILE_ACTIONS_ADDRESS =
-  '0x0693f1a1bba2704e2694b2402dbc5de4e4dcaf03aabbf4d2bad3e47c6a3de4dd'
+const EVOLUTE_DUEL_GAME_ADDRESS = process.env.NEXT_PUBLIC_GAME_ADDRESS || '';
+const EVOLUTE_DUEL_PLAYER_PROFILE_ACTIONS_ADDRESS = process.env.NEXT_PUBLIC_PLAYER_PROFILE_ADDRESS || '';
 
 export interface Transaction {
     contractAddress: string;
     entrypoint: string;
     calldata: string[];
+}
+
+export const create_game = () => {
+    return {
+        contractAddress: EVOLUTE_DUEL_GAME_ADDRESS,
+        entrypoint: "create_game",
+        calldata: [],
+    } as Transaction;
+}
+
+export const create_game_from_snapshot = (snapshotId: string) => {
+  return {
+      contractAddress: EVOLUTE_DUEL_GAME_ADDRESS,
+      entrypoint: "create_game_from_snapshot",
+      calldata: [snapshotId],
+  } as Transaction;
+}
+
+export const create_snapshot = (boardId: string, moveNumber: string) => {
+  return {
+      contractAddress: EVOLUTE_DUEL_GAME_ADDRESS,
+      entrypoint: "create_snapshot",
+      calldata: [boardId, moveNumber],
+  } as Transaction;
+}
+
+export const finish_game = (boardId: string) => {
+  return {
+      contractAddress: EVOLUTE_DUEL_GAME_ADDRESS,
+      entrypoint: "finish_game",
+      calldata: [boardId],
+  } as Transaction;
+}
+
+export const join_game = (hostPlayer: string) => {
+  return {
+      contractAddress: EVOLUTE_DUEL_GAME_ADDRESS,
+      entrypoint: "join_game",
+      calldata: [hostPlayer],
+  } as Transaction;
+}
+
+export const make_move = (jokerTile: string, rotation: string, col: string, row: string) => {
+  return {
+      contractAddress: EVOLUTE_DUEL_GAME_ADDRESS,
+      entrypoint: "make_move",
+      calldata: [jokerTile, rotation, col, row],
+  } as Transaction;
+}
+
+export const skip_move = () => {
+  return {
+      contractAddress: EVOLUTE_DUEL_GAME_ADDRESS,
+      entrypoint: "skip_move",
+      calldata: [],
+  } as Transaction;
+}
+
+export const cancel_game = () => {
+  return {
+      contractAddress: EVOLUTE_DUEL_GAME_ADDRESS,
+      entrypoint: "cancel_game",
+      calldata: [],
+  } as Transaction;
+}
+
+
+export const active_skin = () => {
+  return {
+      contractAddress: EVOLUTE_DUEL_GAME_ADDRESS,
+      entrypoint: "active_skin",
+      calldata: [],
+  } as Transaction;
+}
+
+
+export const balance = () => {
+  return {
+      contractAddress: EVOLUTE_DUEL_GAME_ADDRESS,
+      entrypoint: "balance",
+      calldata: [],
+  } as Transaction;
+}
+
+export const become_bot = () => {
+  return {
+      contractAddress: EVOLUTE_DUEL_GAME_ADDRESS,
+      entrypoint: "become_bot",
+      calldata: [],
+  } as Transaction;
+}
+
+export const change_skin = (skinId: string) => {
+  return {
+      contractAddress: EVOLUTE_DUEL_PLAYER_PROFILE_ACTIONS_ADDRESS,
+      entrypoint: "change_skin",
+      calldata: [skinId],
+  } as Transaction;
 }
 
 export const change_username = (newUsername: string) => {
@@ -15,4 +111,12 @@ export const change_username = (newUsername: string) => {
         entrypoint: "change_username",
         calldata: [newUsername],
     } as Transaction;
+}
+
+export const username = () => {
+  return {
+      contractAddress: EVOLUTE_DUEL_PLAYER_PROFILE_ACTIONS_ADDRESS,
+      entrypoint: "username",
+      calldata: [],
+  } as Transaction;
 }
--- mage-duel-nextjs/lib/unity-connector.ts
+++ mage-duel-nextjs/lib/unity-connector.ts
@@ -2,7 +2,7 @@
 
 import { UnityWindow } from "../components/UnityPlayer";
 import { ControllerWindow } from "../components/WalletConnector";
-import { change_username } from "./transactions";
+import { active_skin, balance, become_bot, cancel_game, change_skin, change_username, create_game, create_game_from_snapshot, create_snapshot, finish_game, join_game, make_move, skip_move, username } from "./transactions";
 import { Transaction } from "./transactions";
 const unityReciver = "WrapperTester";
 
@@ -20,9 +20,83 @@ export default class UnityConnector {
         return tx_hash;
     }
 
+    public SendEvent = (event: string) => {
+        const win = window as UnityWindow;
+        const gameInstance = win.gameInstance;
+        gameInstance.SendMessage(unityReciver, event, "");
+    }
+
+    // -- Game actions --
+    public CreateGame = async () => {
+        const tx = create_game();
+        return this.ExecuteTransaction(tx);
+    }
+
+    public CreateGameFromSnapshot = async (snapshotId: string) => {
+        const tx = create_game_from_snapshot(snapshotId);
+        return this.ExecuteTransaction(tx);
+    }
+
+    public CreateSnapshot = async (boardId: string, moveNumber: string) => {
+        const tx = create_snapshot(boardId, moveNumber);
+        return this.ExecuteTransaction(tx);
+    }
+
+    public FinishGame = async (boardId: string) => {
+        const tx = finish_game(boardId);
+        return this.ExecuteTransaction(tx);
+    }
+
+    public JoinGame = async (hostPlayer: string) => {
+        const tx = join_game(hostPlayer);
+        return this.ExecuteTransaction(tx);
+    }
+
+    public MakeMove = async (jokerTile: string, rotation: string, col: string, row: string) => {
+        const tx = make_move(jokerTile, rotation, col, row);
+        return this.ExecuteTransaction(tx);
+    }
+    
+    public SkipMove = async () => {
+        const tx = skip_move();
+        return this.ExecuteTransaction(tx);
+    }
+
+    public CancelGame = async () => {
+        const tx = cancel_game();
+        return this.ExecuteTransaction(tx);
+    }
+    
+    
     // -- Player profile actions --
+
+    public ActiveSkin = async () => {
+        const tx = active_skin();
+        return this.ExecuteTransaction(tx);
+    }
+
+    public Balance = async () => {
+        const tx = balance();
+        return this.ExecuteTransaction(tx);
+    }
+
+    public BecomeBot = async () => {
+        const tx = become_bot();
+        return this.ExecuteTransaction(tx);
+    }
+
+    public ChangeSkin = async (skinId: string) => {
+        const tx = change_skin(skinId);
+        return this.ExecuteTransaction(tx);
+    }
+
     public ChangeUsername = async (newUsername: string) => {
         const tx = change_username(newUsername);
+        return this.ExecuteTransaction(tx);
+    }
+
+    public Username = async () => {
+        const tx = username();
         return this.ExecuteTransaction(tx);
     }
 
