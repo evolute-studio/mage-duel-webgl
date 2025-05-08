ee41e04d26703af75ab7d46223607e2a427bf249
liyard-tls <mckinley55X@gmail.com>
2025-05-08 16:24:57 +0300

    minor changes

--- mage-duel-nextjs/components/UnityPlayer.tsx
+++ mage-duel-nextjs/components/UnityPlayer.tsx
@@ -31,7 +31,7 @@ export default function UnityPlayer() {
     const containerRef = useRef<HTMLDivElement>(null)
     const canvasRef = useRef<HTMLCanvasElement>(null)
     const projectId = "mageduel-webgl"
-    const version = "1.2.22t"
+    const version = "1.3.0t"
     const compression = ".br"
     const is_compressed = false
 
