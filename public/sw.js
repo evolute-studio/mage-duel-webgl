c54d1c672c98bdbf29b3538dcab2d622c142f962
Daniel | Evolute <semirazov94@gmail.com>
2025-05-10 20:21:23 +0100

    update manifest

--- public/sw.js
+++ public/sw.js
@@ -1,6 +1,6 @@
 // This is the service worker with the Cache-first network strategy.
 
-const CACHE = "mage-duel-pwa-cache-v5";
+const CACHE = "mage-duel-pwa-cache-v6";
 const precacheResources = [
   "/",
   "/favicon.ico",
