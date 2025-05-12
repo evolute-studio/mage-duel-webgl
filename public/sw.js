bc5f5803152ef72bc33fdbe8b0fbf5a12b074d87
Daniel | Evolute <semirazov94@gmail.com>
2025-05-12 16:26:19 +0100

    change sw ver

--- public/sw.js
+++ public/sw.js
@@ -1,6 +1,6 @@
 // This is the service worker with the Cache-first network strategy.
 
-const CACHE = "mage-duel-pwa-cache-v8";
+const CACHE = "mage-duel-pwa-cache-v9";
 const precacheResources = [
   "/",
   "/favicon.ico",
