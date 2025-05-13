44025b3d962c53de12eff45870664632efcd2706
liyard-tls <mckinley55X@gmail.com>
2025-05-14 01:35:08 +0300

    v1.3.13

--- components/UnityPlayer.tsx
+++ components/UnityPlayer.tsx
@@ -31,7 +31,7 @@ export default function UnityPlayer() {
     const containerRef = useRef<HTMLDivElement>(null)
     const canvasRef = useRef<HTMLCanvasElement>(null)
     const projectId = "mageduel-webgl"
-    const version = "1.3.12"
+    const version = "1.3.13"
     const compression = ".br"
     const is_compressed = true
 
