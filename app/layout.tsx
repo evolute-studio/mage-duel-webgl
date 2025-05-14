0163794f6738df8a4c7c54f4bef6367b358a6a97
Daniel | Evolute <semirazov94@gmail.com>
2025-05-14 15:34:24 +0100

    fix

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,5 @@
 import "./globals.css";
-import type { Metadata, Viewport } from "next";
+import type { Metadata } from "next";
 
 export const metadata: Metadata = {
   title: "Evolute Kingdom: Mage Duel",
