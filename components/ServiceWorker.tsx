4af564da8d91f27242c7feb177c39edbc14e5be2
Daniel | Evolute <semirazov94@gmail.com>
2025-05-14 20:11:31 +0100

    fix

--- components/ServiceWorker.tsx
+++ components/ServiceWorker.tsx
@@ -109,7 +109,7 @@ export default function ServiceWorker() {
       window.indexedDB
         .databases()
         .then((r) => {
-          for (var i = 0; i < r.length; i++)
+          for (let i = 0; i < r.length; i++)
             window.indexedDB.deleteDatabase(r[i].name!);
         })
         .then(() => {
