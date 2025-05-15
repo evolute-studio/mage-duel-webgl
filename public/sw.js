8f86006c273b30f817e520f63933a0a39b23d69a
Daniel | Evolute <semirazov94@gmail.com>
2025-05-15 15:08:50 +0100

    tyr fix

--- public/sw.js
+++ public/sw.js
@@ -1,6 +1,6 @@
 // This is the service worker with the Cache-first network strategy.
 
-const CACHE = "mage-duel-pwa-cache-v1318v3";
+const CACHE = "mage-duel-pwa-cache-v1318v4";
 
 const precacheResources = [
   "/",
