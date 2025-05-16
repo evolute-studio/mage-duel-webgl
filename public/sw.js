d818915be8fce76e1f2a7650884f4d7029b3bd8c
Daniel | Evolute <semirazov94@gmail.com>
2025-05-16 13:31:54 +0100

    update sw

--- public/sw.js
+++ public/sw.js
@@ -1,6 +1,6 @@
 // This is the service worker with the Cache-first network strategy.
 
-const CACHE = "mage-duel-pwa-cache-v1320v1";
+const CACHE = "mage-duel-pwa-cache-v1320v2";
 
 const precacheResources = [
   "/",
