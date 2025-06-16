09351a73635f60cec413b01a5665159d7c2fd2ec
liyard-tls <mckinley55X@gmail.com>
2025-06-16 16:47:13 +0300

    v1.4.9

--- components/UnityPlayer.tsx
+++ components/UnityPlayer.tsx
@@ -43,7 +43,7 @@ export default function UnityPlayer({
 }) {
   const canvasRef = useRef<HTMLCanvasElement>(null);
   const projectId = "mageduel-webgl";
-  const version = "1.4.8";
+  const version = "1.4.9";
   const compression = ".br";
   const is_compressed = false;
   const [gameLoaded, setGameLoaded] = useState(false);
