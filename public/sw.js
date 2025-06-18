7d8df29ea66b75a07b17daf68860548a5d72da10
liyard-tls <mckinley55X@gmail.com>
2025-06-18 12:04:00 +0300

    v1.4.12

--- public/sw.js
+++ public/sw.js
@@ -1,6 +1,6 @@
 // This is the service worker with the Cache-first network strategy.
 
-const CACHE = "mage-duel-pwa-cache-v1411v1";
+const CACHE = "mage-duel-pwa-cache-v1412v1";
 
 const precacheResources = [
   "/",
