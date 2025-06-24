0a081a8110bc507aa57d1f7cad76c7feb0472f36
Daniel | Evolute <semirazov94@gmail.com>
2025-06-24 15:32:35 +0100

    update content-type

--- next.config.ts
+++ next.config.ts
@@ -65,6 +65,15 @@ const nextConfig: NextConfig = {
           },
         ],
       },
+      {
+        source: "/.well-known/apple-app-site-association",
+        headers: [
+          {
+            key: "Content-Type",
+            value: "application/json",
+          },
+        ],
+      },
 
       {
         source: "/(.*)",
