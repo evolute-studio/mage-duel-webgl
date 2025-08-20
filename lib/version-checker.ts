01b3fdf4c4f7432d2d31c8fdef9917a1e2e347ed
liyard-tls <mckinley55X@gmail.com>
2025-08-20 14:03:15 +0300

    Deploy version 1.4.142

--- lib/version-checker.ts
+++ lib/version-checker.ts
@@ -1,6 +1,6 @@
 export const NEED_TO_LOGOUT_KEY = 'NEED_TO_LOGOUT';
 
-export const GameVersion = "1.4.141";
+export const GameVersion = "1.4.142";
 
 export function IsNewVersion(): boolean {
     const gameVersion = process.env.NEXT_PUBLIC_SLOT_DATA_VERSION || "";
